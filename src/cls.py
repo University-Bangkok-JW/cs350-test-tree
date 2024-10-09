436d4e771ebc8cb846273b1fe0bd28ce65234d9d
JetsadaWijit <jetsadawijit@outlook.com>
2024-10-09 16:23:06 +0700

    Fix space, key, logic

--- src/cls.py
+++ src/cls.py
@@ -12,19 +12,19 @@ class ArrayTree:
     def arrayToTree(self, arr):
         if not arr:
             return None
-        self.data = arr
+
+        # Handle incomplete arrays gracefully (suggestion from ratings)
+        self.children = [arr[i] if i < len(arr) else None for i in range(len(arr))]
         self.root = self._arrayToTree(0)
+        return self.root  # Return the root for convenience
 
     def _arrayToTree(self, index):
-        if index < len(self.children) or self.children[index] is None:
+        if index >= len(self.children) or self.children[index] is None:
             return None
-        
-        root = Tree(self.children[index])
-        # Left child = 2*i + 1
-        root.left = self._arrayToTree(2*index + 1)
-        # Right child = 2*i + 2
-        root.right = self._arrayToTree(2*index + 2)
 
+        root = Tree(self.children[index])
+        root.left = self._arrayToTree(2 * index + 1)
+        root.right = self._arrayToTree(2 * index + 2)
         return root
 
     def insert(self, data):
@@ -38,10 +38,12 @@ class ArrayTree:
     def insertAt(self, index, data):
         if index < 0:
             return
-        
-        while len(self.children) <= index:
-            self.children.append(None)
-    
+
+        # Expand the children list efficiently using extend() (suggestion)
+        self.children.extend([None] * (index - len(self.children) + 1))
+        self.children[index] = data
+        self.root = self._arrayToTree(0)
+
     def printInorder(self):
         def _inorder(node):
             if node:
@@ -49,9 +51,9 @@ class ArrayTree:
                 print(node.data, end=' ')
                 _inorder(node.right)
 
-            print('Inorder Traversal:', end=' ')
-            _inorder(self.root)
-            print()
+        print('Inorder Traversal:', end=' ')
+        _inorder(self.root)
+        print()
 
     def printPreorder(self):
         def _preorder(node):
@@ -60,9 +62,9 @@ class ArrayTree:
                 _preorder(node.left)
                 _preorder(node.right)
 
-            print('Preorder Traversal:', end=' ')
-            _preorder(self.root)
-            print()
+        print('Preorder Traversal:', end=' ')
+        _preorder(self.root)
+        print()
 
     def printPostorder(self):
         def _postorder(node):
@@ -71,9 +73,9 @@ class ArrayTree:
                 _postorder(node.right)
                 print(node.data, end=' ')
 
-            print('Postorder Traversal:', end=' ')
-            _postorder(self.root)
-            print()
+        print('Postorder Traversal:', end=' ')
+        _postorder(self.root)
+        print()
 
     def printArr(self):
         print('Array Representation:', self.children)
